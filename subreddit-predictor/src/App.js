06357e1844aea98e9855415ee441d74f5750e519
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-06-17 20:23:47 -0700

    test run

--- subreddit-predictor/src/App.js
+++ subreddit-predictor/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Subreddit-Predictor Forms</div>;
+  return <div className="App">Subreddit-Predictor Forms 12345</div>;
 }
 
 export default App;
