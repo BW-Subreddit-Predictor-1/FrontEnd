80d408630b192fb9e41e01af4aa920e1c446c69a
Donna Ballew <ballewdd@gmail.com>
2020-06-19 01:38:24 -0700

    added UserHomePage.js; needs navbar

---
+++ subreddit-predictor/src/components/UserHomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "./Search.js";
+import UserPost from "./UserPost.js";
+
+const UserHomePage = () => {
+  return (
+    <>
+      <Search />
+      <UserPost />
+    </>
+  );
+};
+
+export default UserHomePage;
