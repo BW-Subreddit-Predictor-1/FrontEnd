56823173192a12e7186b78486972b56a85a386ed
Donna Ballew <ballewdd@gmail.com>
2020-06-19 01:35:32 -0700

    added Signup.js

---
+++ subreddit-predictor/src/components/Signup.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+// import axios from "axios";
+import { Link } from "react-router-dom";
+import { Form, Label, Input, Button, FormGroup } from "reactstrap";
+// import * as yup from "yup";
+
+const Signup = (e) => {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmationPassword: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  // const [loading, setLoading] = useState(true);
+
+  // //Time for Loading Screen
+  // setTimeout(() => {
+  //   setLoading(false);
+  // }, 900);
+
+  return (
+    <>
+      <nav>
+        <div>
+          <h1>Post Here: Subreddit - Predictor</h1>
+          <Link to="/">Home</Link>
+          <Link to="/">About</Link>
+          <Link to="/">Log In</Link>
+          <Link to="/">Sign Up</Link>
+        </div>
+      </nav>
+      <div>
+        <h1>SIGN UP</h1>
+        <h4>
+          By having a Subreddit account, you can join, vote, and comment on all
+          your favorite Subreddit content.
+        </h4>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label>First Name</Label>
+            <Input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              id="firstName"
+              value={user.firstName}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Last Name</Label>
+            <Input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              id="lastName"
+              value={user.lastName}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Email</Label>
+            <Input
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Password</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Password"
+              id="password"
+              value={user.password}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Confirmation Password</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Password"
+              id="password"
+              value={user.password}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <Button type="submit">Sign Up</Button>
+
+          <h3>
+            Already a Redditor? <Link to="/">LOG IN</Link>
+          </h3>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
