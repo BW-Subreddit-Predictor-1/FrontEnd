a1817ba47970660a67ed205f6448e91dd7e8d7ed
Donna Ballew <ballewdd@gmail.com>
2020-06-19 01:25:41 -0700

    App is not rendering correctly; needs correcting

--- subreddit-predictor/src/App.js
+++ subreddit-predictor/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
-
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login.js";
+import Signup from "./components/Signup.js";
+import UserHomePage from "./components/UserHomePage.js";
 
 function App() {
-  return <div className="App">Subreddit-Predictor Forms</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={UserHomePage} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
