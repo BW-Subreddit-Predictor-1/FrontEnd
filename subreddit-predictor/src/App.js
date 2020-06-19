d9f9f4bd77e0db97b6184e51ed0ecd57505c39df
Donna Ballew <ballewdd@gmail.com>
2020-06-19 14:40:19 -0700

    corrected routes

--- subreddit-predictor/src/App.js
+++ subreddit-predictor/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import Login from "./components/Login.js";
 import Signup from "./components/Signup.js";
 import UserHomePage from "./components/UserHomePage.js";
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={UserHomePage} />
-        <Route path="/login" component={Login} />
+        <Route exact path="/" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/userHomePage" component={UserHomePage} />
       </Switch>
     </div>
   );
