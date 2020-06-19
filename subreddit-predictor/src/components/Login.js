9db22bbb7d05e1a0e092ee0e52f735e300717fdc
Donna Ballew <ballewdd@gmail.com>
2020-06-19 01:29:13 -0700

    Added Login.js; may need correcting and lacks styling

--- subreddit-predictor/src/components/Login.js
+++ subreddit-predictor/src/components/Login.js
@@ -1,57 +1,72 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { Link } from "react-router-dom";
-import * as yup from "yup";
+// import * as yup from "yup";
 
-const [login, setlogin] = useState({
-  email: "",
-  password: "",
-});
+const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-// const [loading, setLoading] = useState(true);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-// //Time for Loading Screen
-// setTimeout(() => {
-//   setLoading(false);
-// }, 900);
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-return (
-  <>
-    <nav>
+  // const [loading, setLoading] = useState(true);
+
+  // //Time for Loading Screen
+  // setTimeout(() => {
+  //   setLoading(false);
+  // }, 900);
+
+  return (
+    <>
+      <nav>
+        <div>
+          <h1>Post Here: Subreddit - Predictor</h1>
+          <Link to="/">Home</Link>
+          <Link to="/">About</Link>
+          <Link to="/">Log In</Link>
+          <Link to="/">Sign Up</Link>
+        </div>
+      </nav>
       <div>
-        <h1>Post Here: Subreddit - Predictor</h1>
-        <Link to="/">Home</Link>
-        <Link to="/">About</Link>
-        <Link to="/">Log In</Link>
-        <Link to="/">Sign Up</Link>
-      </div>
-    </nav>
-    <div>
-      <form onSubmit={submit}>
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          value="login.email"
-          id="email"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="password"
-          placeholder="Password"
-          value="login.password"
-          id="password"
-          onChange={handleChange}
-        />
-        <button>Log In</button>
+        <h1>LOG IN</h1>
+        <form onSubmit={handleSubmit}>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            id="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            id="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <button type="submit">Log In</button>
 
-        <h3>Forgot your username or password?</h3>
-        <br />
-        <h3>New to Reddit? SIGN UP</h3>
-      </form>
-    </div>
-  </>
-);
+          <h3>Forgot your email or password?</h3>
+          <br />
+          <h3>
+            New to Reddit? <Link to="/">SIGN UP</Link>
+          </h3>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Login;
