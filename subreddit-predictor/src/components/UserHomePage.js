aafd35629a8ecc10aaef8177397452c9b8956f96
Donna Ballew <ballewdd@gmail.com>
2020-06-19 23:12:23 -0700

    added <h1> to userhomePage

--- subreddit-predictor/src/components/UserHomePage.js
+++ subreddit-predictor/src/components/UserHomePage.js
@@ -1,48 +1,45 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./Search.js";
 import UserPost from "./UserPost.js";
-import { Link } from 'react-router-dom';
-import { RedditContext } from '../contexts/RedditContext';
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import { RedditContext } from "../contexts/RedditContext";
+import axios from "axios";
 
 const UserHomePage = () => {
-
   const initialPost = {
-    title: 'My first reddit post',
-    post: 'Reddit is kinda cool'
-  }
+    title: "My first reddit post",
+    post: "Reddit is kinda cool",
+  };
 
-  const [post, setPost ] =useState(initialPost);
+  const [post, setPost] = useState(initialPost);
 
   useEffect(() => {
     axios
-      .get('http://localhost:5000/api/post')
-      .then(res => {
-        setPost(res.data)
+      .get("http://localhost:5000/api/post")
+      .then((res) => {
+        setPost(res.data);
       })
-      .catch(err => {
-        console.error(err.message, err.response)
-      })
-  }, [])
+      .catch((err) => {
+        console.error(err.message, err.response);
+      });
+  }, []);
 
   return (
     <>
-
-       <nav>
-          <h1>Post Here: Subreddit - Predictor</h1>
+      <nav>
+        <h1>Post Here: Subreddit - Predictor</h1>
         <Link to={"/"}>Home</Link>
         <Link to={"/"}>About</Link>
         <Link to={"/"}>Log out</Link>
         <Link to={"/signup"}>Sign Up</Link>
       </nav>
-      
 
-<RedditContext.Provider value={post, setPost}>
-      <Search />
-      <br />
-      <UserPost />
-</RedditContext.Provider>
-   
+      <RedditContext.Provider value={(post, setPost)}>
+        <h1 className="userHomePgh1">Profile Post Page</h1>
+        <Search />
+        <br />
+        <UserPost />
+      </RedditContext.Provider>
     </>
   );
 };
