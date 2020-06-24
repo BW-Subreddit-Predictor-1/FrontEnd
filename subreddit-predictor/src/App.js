95c1eff729ed38b4c6bf04b8695d0a907559a715
Donna Ballew <ballewdd@gmail.com>
2020-06-24 00:18:28 -0700

    added path=/login to line 28

--- subreddit-predictor/src/App.js
+++ subreddit-predictor/src/App.js
@@ -4,26 +4,28 @@ import { Route, Switch, Link } from "react-router-dom";
 import Login from "./components/Login.js";
 import Signup from "./components/Signup.js";
 import UserHomePage from "./components/UserHomePage.js";
-import PrivateRoute from './components/PrivateRoute';
-import { RedditContext } from './contexts/RedditContext';
+import PrivateRoute from "./components/PrivateRoute";
+import { RedditContext } from "./contexts/RedditContext";
 
 function App() {
-
-  const [ loggedState, setLoggedState ] = useState(false, JSON.parse(localStorage.getItem('loggedState')));
+  const [loggedState, setLoggedState] = useState(
+    false,
+    JSON.parse(localStorage.getItem("loggedState"))
+  );
 
   const logOut = () => {
-    localStorage.removeItem('token')
-    localStorage.setItem('loggedState', false)
-  }
+    localStorage.removeItem("token");
+    localStorage.setItem("loggedState", false);
+  };
 
-  return ( 
+  return (
     <div className="App">
       <Switch>
-        <RedditContext.Provider value={loggedState, logOut, setLoggedState }>
+        <RedditContext.Provider value={(loggedState, logOut, setLoggedState)}>
           {/* <PrivateRoute exact path="/userHomePage" component={UserHomePage} /> */}
-          <Route path='/userHomePage' component={UserHomePage} />
+          <Route path="/userHomePage" component={UserHomePage} />
           <Route path="/signup" component={Signup} />
-          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
         </RedditContext.Provider>
       </Switch>
     </div>
