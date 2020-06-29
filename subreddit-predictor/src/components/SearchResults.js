06b5b666f17643d965017ce6ad457e1da8db79fa
Donna Ballew <ballewdd@gmail.com>
2020-06-28 20:29:57 -0700

    commented out code

--- subreddit-predictor/src/components/SearchResults.js
+++ subreddit-predictor/src/components/SearchResults.js
@@ -1,13 +1,54 @@
-import React from 'react';
-
-
+import React, { useState, useContext } from "react";
+import { Label, Form, Container, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import { RedditContext } from "../contexts/RedditContext";
+import UserPost from "./UserPost";
 
 const SearchResults = () => {
-    return (
-        <div>
-            <h1 style={{color: 'white'}}>Search Results</h1>
-        </div>
-    )
-}
+  // const post = useContext(RedditContext);
+  const [post, setPost] = useState([]);
+  const addPost = (newPost) => {
+    setPost([...post, newPost]);
+  };
 
+  const initialPost = [
+    {
+      title: "My first reddit post",
+      body: "Reddit is kinda cool",
+    },
+    {
+      title: "LambdaSchool:  The Official Lambda subreddit",
+      body: "Welcome to r/lambdaschool, the official Lambda School subreddit.",
+    },
+    {
+      title: "webdev: reddit for web developers",
+      body:
+        "A community dedicated to all things web development: both front-end and back-end.",
+    },
+  ];
+
+  return (
+    <>
+      <div className="Searches">
+        <Label>Search Results</Label>
+        <initialPost addPost={addPost} />
+        {initialPost.map((post, i) => (
+          // console.log("index"),
+          <Container
+            style={{ width: "40%", margin: "0 auto", color: "white" }}
+            key={i++}
+          >
+            <Row>Title: {post.title}</Row>
+
+            {/* <br />
+            Body: {post.body}
+            <br /> */}
+            <Button>Delete Post</Button>
+          </Container>
+        ))}
+        ;
+      </div>
+    </>
+  );
+};
 export default SearchResults;
